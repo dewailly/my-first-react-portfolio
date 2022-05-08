4bb669de301bbdb02ed26bbb8dc15ba60da1cff9
Dewailly Guillaume <101576944+dewailly@users.noreply.github.com>
2022-05-09 00:18:37 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import "./App.css";
-import NavBar from "./Components/NavBar";
-import HomePage from "./Pages/HomePage";
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <div className="sidebar">
-        <NavBar />
-      </div>
-      <div className="main-content">
-        <div className="content">
-          <HomePage />
-        </div>
-      </div>
-    </div>
-  );
+    <>
+      
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
